56f954fe7df7558b531facefc28217bcd198ea78
Serena <rotondaserena3@gmail.com>
2026-05-10 19:30:03 -0300

    add users CRUD base structure

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import testRoutes from "./routes/test.routes.js";
+import usersRoutes from "./routes/users.routes.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(errorMiddleware);
 app.use("/api", testRoutes);
+app.use("/api/users", usersRoutes);
+
+app.use(errorMiddleware);
 
 export default app;
