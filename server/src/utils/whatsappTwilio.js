ff289e9f154e0f701af206d5f1c635bc78729592
unknown <sdjsm2003@gmail.com>
2026-05-22 16:15:17 -0400

    Task 8, part 2.2

--- server/src/utils/whatsappTwilio.js
+++ server/src/utils/whatsappTwilio.js
@@ -6,7 +6,7 @@ export const whatsappMessage = async (theToNumber, theMessage) => {
   try {
     const client = twilio(process.env.ACCOUNT_SSID, process.env.AUTH_TWILIO_TOKEN);
     return await client.messages.create({
-        from: "whatsapp:+14155238886",
+        from: "whatsapp:+13185692652",
         to: `whatsapp:${theToNumber}`,
         body: theMessage,
       });
